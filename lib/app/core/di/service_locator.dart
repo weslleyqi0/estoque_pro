9d1b3541c338b768783cca7275e166b9b4a9b936
Weslley Santos <weslleyqi0@gmail.com>
2026-07-27 15:24:52 -0300

    feat(core): register SuppliersRepository and update service locator

--- lib/app/core/di/service_locator.dart
+++ lib/app/core/di/service_locator.dart
@@ -2,6 +2,9 @@ import 'package:estoque_pro/app/core/services/firebase_database_service.dart';
 import 'package:estoque_pro/app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:estoque_pro/app/features/auth/data/service/auth_service.dart';
 import 'package:estoque_pro/app/features/auth/data/service/biometric_service.dart';
+import 'package:estoque_pro/app/features/suppliers/data/repositories/suppliers_repository_impl.dart';
+import 'package:estoque_pro/app/features/suppliers/domain/entities/supplier_entity.dart';
+import 'package:estoque_pro/app/features/suppliers/domain/repositories/suppliers_repository.dart';
 import 'package:estoque_pro/app/features/users/domain/entities/user_entity.dart';
 import 'package:estoque_pro/app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:estoque_pro/app/features/auth/presentation/viewmodels/auth_viewmodel.dart';
@@ -31,6 +34,7 @@ void setupServiceLocator() {
 
   // Services / Data Sources
   registerDatabaseService<UserEntity>('users');
+  registerDatabaseService<SupplierEntity>('suppliers');
 
   // Services
   getIt.registerLazySingleton<AuthService>(() => AuthService());
@@ -49,6 +53,12 @@ void setupServiceLocator() {
 
   getIt.registerLazySingleton<UsersRepository>(
     () => UsersRepositoryImpl(getIt<FirebaseDatabaseService<UserEntity>>()),
+  );
+
+  getIt.registerLazySingleton<SuppliersRepository>(
+    () => SuppliersRepositoryImpl(
+      getIt<FirebaseDatabaseService<SupplierEntity>>(),
+    ),
   );
 
   // ViewModels
