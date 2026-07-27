4d8700b710e0f93498d2af8da14abc40e4a18a20
Weslley Santos <weslleyqi0@gmail.com>
2026-07-27 15:25:05 -0300

    feat(main): enable persistence for Firebase Database

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:estoque_pro/app/core/di/service_locator.dart';
 import 'package:estoque_pro/app/core/router/app_router.dart';
 import 'package:estoque_pro/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseDatabase.instance.setPersistenceEnabled(true);
+
   setupServiceLocator();
   runApp(const MyApp());
 }
