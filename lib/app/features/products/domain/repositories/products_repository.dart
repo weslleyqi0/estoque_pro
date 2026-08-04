83caf1523115e9db85dff9c38436a14b96e1d465
Weslley Santos <weslleyqi0@gmail.com>
2026-08-03 22:41:01 -0300

    feat(products): create ProductsRepository interface for product management

---
+++ lib/app/features/products/domain/repositories/products_repository.dart
@@ -0,0 +1,9 @@
+import 'package:estoque_pro/app/features/products/domain/entities/product_entity.dart';
+
+abstract class ProductsRepository {
+  Stream<List<ProductEntity>> watchAll();
+  Future<List<ProductEntity>> getAll();
+  Future<void> save(ProductEntity product);
+  Future<void> update(ProductEntity product);
+  Future<void> delete(String id);
+}
