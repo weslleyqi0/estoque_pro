bd178c206aafb5a0e7d0757b776d370c5bfeb311
Weslley Santos <weslleyqi0@gmail.com>
2026-08-22 15:16:24 -0300

    fix(products): remove obsolete delete method from ProductsRepository

--- lib/app/features/products/domain/repositories/products_repository.dart
+++ lib/app/features/products/domain/repositories/products_repository.dart
@@ -6,7 +6,6 @@ abstract class ProductsRepository {
   Future<List<ProductEntity>> getAll();
   Future<void> save(ProductEntity product);
   Future<void> update(ProductEntity product);
-  Future<void> delete(String id);
   Future<void> archive(String id);
   Future<void> unarchive(String id);
   Future<void> deletePermanently(String id);
