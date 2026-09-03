0dedb27d2bcb6db10e40d95f05148ec7d4ee97bc
Weslley Santos <weslleyqi0@gmail.com>
2026-09-03 20:16:16 -0300

    feat(splash): support flavor-aware logo and app title on splash screen

--- lib/app/core/router/routes/splash_routes.dart
+++ lib/app/core/router/routes/splash_routes.dart
@@ -2,6 +2,7 @@ import 'package:estoque_pro/app/core/di/service_locator.dart';
 import 'package:estoque_pro/app/core/router/app_routes.dart';
 import 'package:estoque_pro/app/features/auth/presentation/viewmodels/auth_viewmodel.dart';
 import 'package:estoque_pro/app/features/splash/presentation/pages/splash_page.dart';
+import 'package:estoque_pro/config/app_config.dart';
 import 'package:go_router/go_router.dart';
 
 List<GoRoute> splashRoutes() {
@@ -10,6 +11,7 @@ List<GoRoute> splashRoutes() {
       path: AppRoutes.splash,
       builder: (context, state) => SplashPage(
         authViewModel: getIt<AuthViewModel>(),
+        appConfig: getIt<AppConfig>(),
       ),
     ),
   ];
